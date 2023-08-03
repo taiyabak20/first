3a46b1f9fef331f26b35d9b02f712cc87cec4004
taiyabak20 <taiyabak20@gmail.com>
2023-08-04 03:15:34 +0530

    commit 5

--- demo.js
+++
@@ -1,1 +0,0 @@
-console.log("i dont actually hate dancing")
---
+++ index.js
@@ -0,0 +1,18 @@
+// console.dir(document)
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.doctype)
+// console.log(document.all);
+ document.title="forms";
+var headerTitle= document.getElementById("header-title");
+
+headerTitle.style.borderBottom ="solid 3px #ffffff"
+
+var Items= document.getElementsByClassName("title")
+
+
+console.log(Items[1]);
+Items[1].style.fontWeight="bold";
+
+
+
