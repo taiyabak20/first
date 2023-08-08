8a01415abb80bd58a155e75980fe7eee466dcef3
taiyabak20 <taiyabak20@gmail.com>
2023-08-09 00:55:00 +0530

    commit8

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
     // console.log(document.doctype)
     // console.log(document.all);
 
-    document.title="forms";
+    //document.title="forms";
     //var headerTitle= document.getElementById("header-title");
     //headerTitle.style.borderBottom ="solid 3px #000000"
     
@@ -21,7 +21,7 @@
     var listItems = document.getElementsByClassName('list-group-item');
     console.log(listItems[2].innerHTML);
     console.log(listItems)
-    listItems[2].style.backgroundColor='green';
+    //listItems[2].style.backgroundColor='green';
     for( var i=0;i<listItems.length;i++){
 
       listItems[i].style.fontWeight='bold';
@@ -35,5 +35,22 @@ console.log(item5)
     item5[0].style.fontWeight= "bold";
     item5[0].style.listStyle = "none";
 
+var item2 = document.querySelector('li:nth-child(2)');
+
+item2.style.backgroundColor ='green';
+
+var item3 = document.querySelector('li:nth-child(3)');
+
+item3.style.color = 'transparent';
+
+var allItems = document.querySelectorAll('li')
+
+  allItems[1].style.color = 'green';
 
 
+var oddItems = document.querySelectorAll('li:nth-child(odd)');
+
+for(var i=0; i<oddItems.length;i++){
+
+  oddItems[i].style.backgroundColor = 'green';
+}
