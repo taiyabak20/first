f4456fa2cdb24b3df537866129eac6f7ce097a42
taiyabak20 <taiyabak20@gmail.com>
2023-08-08 19:42:08 +0530

    commit6

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-// console.dir(document)
-// console.log(document.domain);
-// console.log(document.URL);
-// console.log(document.doctype)
-// console.log(document.all);
- document.title="forms";
-var headerTitle= document.getElementById("header-title");
+    //console.dir(document)
+    // console.log(document.domain);
+    // console.log(document.URL);
+    // console.log(document.doctype)
+    // console.log(document.all);
 
-headerTitle.style.borderBottom ="solid 3px #ffffff"
+    document.title="forms";
+    //var headerTitle= document.getElementById("header-title");
+    //headerTitle.style.borderBottom ="solid 3px #000000"
+    
+    var header=document.getElementById("main-header");
+    
+    header.style.borderBottom="solid 3px #000000"
+    var Items= document.getElementsByClassName("title")
+    
+    
+    
+    console.log(Items[0].innerHTML);
+    Items[0].style.color="green";
+    
+    var listItems = document.getElementsByClassName('list-group-item');
+    console.log(listItems[2].innerHTML);
+    listItems[2].style.backgroundColor='green';
+    for( var i=0;i<listItems.length;i++){
 
-var Items= document.getElementsByClassName("title")
+      listItems[i].style.fontWeight='bold';
 
-
-console.log(Items[1]);
-Items[1].style.fontWeight="bold";
-
-
+    }
+    
 
